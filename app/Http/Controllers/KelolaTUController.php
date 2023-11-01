56c52a0ef1618ffad4d31a2816556d3898c77b8c
rivando al rasyid <nougat-tutoria@protonmail.com>
2023-11-01 21:57:14 +0700

    updat

--- app/Http/Controllers/KelolaTUController.php
+++ app/Http/Controllers/KelolaTUController.php
@@ -2,11 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\KelolaTU;
 use RealRashid\SweetAlert\Facades\Alert;
-
-// use App\Models\KelolaTU;
-
 use App\Models\User;
 
 use Illuminate\Http\Request;
@@ -36,58 +32,60 @@ class KelolaTUController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'nama_pekerjaan' => 'required',
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed',
         ]);
 
-        $kelola_TU = new User();
-        $kelola_TU->nama_pekerjaan = $request->nama_pekerjaan;
-        $kelola_TU->save();
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
 
-        Alert::success('Success', 'Data Saved Successfully');
+        Alert::success('Success', 'User Created Successfully');
         return redirect()->route('kelola_tu.index');
     }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(User $kelola_TU)
-    {
-        // Implementation for showing a specific resource
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit($id)
     {
         $data = User::find($id);
-
         return view('dashboards.kelola_tu.edit', compact('data'));
     }
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, User $kelola_TU)
+
+    public function update(Request $request, $id)
     {
         $request->validate([
-            'nama_pekerjaan' => 'required',
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email,' . $id,
+            'password' => 'nullable|min:6|confirmed',
         ]);
 
-        $kelola_TU->update([
-            'nama_pekerjaan' => $request->nama_pekerjaan,
-        ]);
+        $user = User::find($id);
+        if ($user) {
+            $user->name = $request->name;
+            $user->email = $request->email;
+            if ($request->password) {
+                $user->password = bcrypt($request->password);
+            }
+            $user->save();
 
-        Alert::success('Success', 'Data Updated Successfully');
-        return redirect()->route('kelola_tu.index');
+            Alert::success('Success', 'Data Updated Successfully');
+            return redirect()->route('kelola_tu.index');
+        }
+
+        Alert::error('Error', 'Failed to update data');
+        return back();
     }
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(User $kelola_TU)
+    public function destroy($id)
     {
-        $kelola_TU->delete();
+        $user = User::find($id);
+        if ($user) {
+            $user->delete();
+            Alert::success('Success', 'User Deleted Successfully');
+            return redirect()->route('kelola_tu.index');
+        }
 
-        Alert::success('Success', 'Data Deleted Successfully');
-        return redirect()->route('kelola_tu.index');
+        Alert::error('Error', 'Failed to delete user');
+        return back();
     }
 }
--- resources/views/dashboards/kelola_tu/edit.blade.php
+++ resources/views/dashboards/kelola_tu/edit.blade.php
@@ -20,64 +20,19 @@
                     @csrf
                     <div class="form-group">
                         <label>Nama User</label>
-                        <input type="text" class="form-control" value="{{ $data->name }}" name="name">
+                        <input type="text" class="form-control" value="{{ $data->name }}" name="name" required>
                     </div>
                     <div class="form-group">
                         <label>Email</label>
-                        <input type="email" class="form-control" value="{{ $data->email }}" name="email">
+                        <input type="email" class="form-control" value="{{ $data->email }}" name="email" required>
                     </div>
                     <div class="form-group">
                         <label>Password</label>
-                        <input type="password" class="form-control" name="password">
+                        <input type="password" class="form-control" name="password" minlength="6">
                     </div>
                     <div class="form-group">
                         <label>Password Confirmation</label>
-                        <input type="password" class="form-control" name="password_confirmation">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Save</button>
-                </form>
-            </div>
-        </div>
-
-    </div>
-@endsection
-
-
-@extends('dashboards.layouts.app')
-@section('content')
-    <div class="container-fluid">
-
-        <!-- Page Heading -->
-        <h1 class="h3 mb-2 text-gray-800">Pkerjaan Orangtua</h1>
-        <p class="mb-4">
-            Edit Data Pekerjaan Orangtua
-        </p>
-
-        <!-- DataTales Example -->
-        <div class="card shadow mb-4">
-            <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary">Form Data Pekerjaan Orangtua</h6>
-            </div>
-            <div class="card-body">
-                <a href="{{ route('pekerjaan_ortu.index') }}" class="btn btn-warning mb-2">Back</a>
-                <form action="{{ route('kelola_tu.update', $data->id) }}" method="POST">
-                    @method('PATCH')
-                    @csrf
-                    <div class="form-group">
-                        <label>Nama User</label>
-                        <input type="text" class="form-control" value="{{ $data->name }}" name="name">
-                    </div>
-                    <div class="form-group">
-                        <label>Email</label>
-                        <input type="email" class="form-control" value="{{ $data->email }}" name="email">
-                    </div>
-                    <div class="form-group">
-                        <label>Password</label>
-                        <input type="password" class="form-control" name="password">
-                    </div>
-                    <div class="form-group">
-                        <label>Password Confirmation</label>
-                        <input type="password" class="form-control" name="password_confirmation">
+                        <input type="password" class="form-control" name="password_confirmation" minlength="6">
                     </div>
                     <button type="submit" class="btn btn-primary">Save</button>
                 </form>
