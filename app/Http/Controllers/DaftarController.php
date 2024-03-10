6a97087fba54c19748721852dbe1bb5bae289b50
rivando al rasyid <nougat-tutorial@protonmail.com>
2024-03-10 18:11:02 +0700

    upload

--- app/Http/Controllers/DaftarController.php
+++ app/Http/Controllers/DaftarController.php
@@ -15,39 +15,99 @@ use App\Models\TblPekerjaanOrtu;
 use App\Models\TblBiodataOrtu;
 use App\Models\TblHasil;
 use Illuminate\Support\Facades\Storage;
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Process\Process;
+use Illuminate\Support\Facades\Process;
+
 class DaftarController extends Controller
 {
     public function index()
     {
         $pekerjaan_ortu = TblPekerjaanOrtu::all();
-        return view(
-            'home.pendaftaran',
-            compact(
-                'pekerjaan_ortu',
-            )
-        );
+        return view('home.pendaftaran', compact('pekerjaan_ortu'));
     }
 
     public function daftar(Request $request)
     {
         DB::beginTransaction();
 
-        $validator = $this->validateRequest($request);
-
-        if ($validator->fails()) {
-            return redirect()->back()->withErrors($validator->errors());
-        }
-
         try {
-            $ortu = $this->createOrtu($request);
-            $wali = $this->createWali($request);
-            $kartu = $this->createKartu($request);
-            $file = $this->createFile($request);
+            $validator = Validator::make($request->all(), [
+                'nama_depan' => 'required',
+                'nama_belakang' => 'required',
+                'nisn' => 'required|numeric',
+                'nik' => 'required|numeric',
+                'no_kk' => 'required|numeric',
+                'jenis_kelamin' => 'required',
+                'agama' => 'required',
+                'tanggal_lahir' => 'required|date|before:yesterday',
+                'tempat_lahir' => 'required',
+                'asal_sekolah' => 'required',
+                'alamat' => 'required',
+                'nilai_rata_rata' => 'nullable|numeric',
+                'rapor_semester_9' => 'file|mimes:pdf',
+                'rapor_semester_10' => 'file|mimes:pdf',
+                'rapor_semester_11' => 'file|mimes:pdf',
+                'foto' => 'image|max:2048',
+            ]);
 
-            $daftar = $this->createPeserta($request, $ortu->id, $wali->id, $kartu->id, $file->id);
-            $hasil = $this->createHasil($daftar);
+            if ($validator->fails()) {
+                return redirect()->back()->withErrors($validator->errors());
+            }
+
+            $ortu = TblBiodataOrtu::create([
+                'nama_ayah' => $request->nama_ayah,
+                'nama_ibu' => $request->nama_ibu,
+                'no_tlp_ayah' => $request->no_telp_ayah,
+                'no_tlp_ibu' => $request->no_telp_ibu,
+                'id_pekerjaan_ayah' => $request->id_pekerjaan_ayah,
+                'id_pekerjaan_ibu' => $request->id_pekerjaan_ibu,
+            ]);
+
+            $wali = TblBiodataWali::create([
+                'nama_wali' => $request->nama_wali,
+                'no_tlp_wali' => $request->no_telp_wali,
+                'id_pekerjaan_wali' => $request->id_pekerjaan_wali,
+            ]);
+
+            $kartu = TblKartu::create([
+                'kip' => $request->nomor_kip,
+                'kks' => $request->nomor_kks,
+                'kps' => $request->nomor_kps,
+                'pkh' => $request->nomor_pkh,
+            ]);
+
+            $data = [
+                'rapor_semester_9' => $request->hasFile('rapor_semester_9') ? $this->storeFileWithCustomName($request->file('rapor_semester_9'), $request->nama_depan, $request->nama_belakang, 'rapor_semester_9') : null,
+                'rapor_semester_10' => $request->hasFile('rapor_semester_10') ? $this->storeFileWithCustomName($request->file('rapor_semester_10'), $request->nama_depan, $request->nama_belakang, 'rapor_semester_10') : null,
+                'rapor_semester_11' => $request->hasFile('rapor_semester_11') ? $this->storeFileWithCustomName($request->file('rapor_semester_11'), $request->nama_depan, $request->nama_belakang, 'rapor_semester_11') : null,
+                'foto' => $request->hasFile('foto') ? $this->storeFileWithCustomName($request->file('foto'), $request->nama_depan, $request->nama_belakang, 'foto') : null,
+            ];
+            // Remove null values from the data array
+            $filteredData = array_filter($data);
+
+            $file = TblFile::create($filteredData);
+
+            $daftar = TblPesertaPpdb::create([
+                'nama_depan' => $request->nama_depan,
+                'nama_belakang' => $request->nama_belakang,
+                'nisn' => $request->nisn,
+                'nik' => $request->nik,
+                'no_kk' => $request->no_kk,
+                'jenis_kelamin' => $request->jenis_kelamin,
+                'agama' => $request->agama,
+                'tanggal_lahir' => $request->tanggal_lahir,
+                'tempat_lahir' => $request->tempat_lahir,
+                'asal_sekolah' => $request->asal_sekolah,
+                'alamat' => $request->alamat,
+                'id_biodata_ortu' => $ortu->id,
+                'id_biodata_wali' => $wali->id,
+                'id_kartu' => $kartu->id,
+                'id_file' => $file->id,
+            ]);
+
+            // Create TblHasil record
+            TblHasil::create([
+                'nis' => $daftar->id
+            ]);
 
             DB::commit();
 
@@ -59,134 +119,15 @@ class DaftarController extends Controller
             return redirect()->back();
         }
     }
-
-    private function validateRequest($request)
+    private function storeFileWithCustomName($file, $firstName, $lastName, $fileType)
     {
-        return Validator::make($request->all(), [
-            'nama_depan' => 'required',
-            'nama_belakang' => 'required',
-            'nisn' => 'required|numeric',
-            'nik' => 'required|numeric',
-            'no_kk' => 'required|numeric',
-            'jenis_kelamin' => 'required',
-            'agama' => 'required',
-            'tanggal_lahir' => 'required|date|before:yesterday',
-            'tempat_lahir' => 'required',
-            'asal_sekolah' => 'required',
-            'alamat' => 'required',
-            'nilai_rata_rata' => 'nullable|numeric',
-            'rapor_semester_9' => 'file|mimes:pdf',
-            'rapor_semester_10' => 'file|mimes:pdf',
-            'rapor_semester_11' => 'file|mimes:pdf',
-            'foto' => 'image|max:2048', // Limit image size to 2MB
-
-        ]);
+        $extension = $file->getClientOriginalExtension();
+        $fileName = $fileType . '_' . $firstName . '_' . $lastName . '.' . $extension;
+        return $file->storeAs('uploads', $fileName, 'public');
     }
-
-    private function createPeserta($request, $idBiodataOrtu, $idBiodataWali, $idKartu, $idFile)
-    {
-        return TblPesertaPpdb::create([
-            'nama_depan' => $request->nama_depan,
-            'nama_belakang' => $request->nama_belakang,
-            'nisn' => $request->nisn,
-            'nik' => $request->nik,
-            'no_kk' => $request->no_kk,
-            'jenis_kelamin' => $request->jenis_kelamin,
-            'agama' => $request->agama,
-            'tanggal_lahir' => $request->tanggal_lahir,
-            'tempat_lahir' => $request->tempat_lahir,
-            'asal_sekolah' => $request->asal_sekolah,
-            'alamat' => $request->alamat,
-            'id_biodata_ortu' => $idBiodataOrtu,
-            'id_biodata_wali' => $idBiodataWali,
-            'id_kartu' => $idKartu,
-            'id_file' => $idFile,
-        ]);
-    }
-
-    private function createOrtu($request)
-    {
-        return TblBiodataOrtu::create([
-            'nama_ayah' => $request->nama_ayah,
-            'nama_ibu' => $request->nama_ibu,
-            'no_tlp_ayah' => $request->no_telp_ayah,
-            'no_tlp_ibu' => $request->no_telp_ibu,
-            'id_pekerjaan_ayah' => $request->id_pekerjaan_ayah,
-            'id_pekerjaan_ibu' => $request->id_pekerjaan_ibu,
-        ]);
-    }
-
-    private function createWali($request)
-    {
-        return TblBiodataWali::create([
-            'nama_wali' => $request->nama_wali,
-            'no_tlp_wali' => $request->no_telp_wali,
-            'id_pekerjaan_wali' => $request->id_pekerjaan_wali,
-        ]);
-    }
-
-    private function createKartu($request)
-    {
-        return TblKartu::create([
-            'kip' => $request->nomor_kip,
-            'kks' => $request->nomor_kks,
-            'kps' => $request->nomor_kps,
-            'pkh' => $request->nomor_pkh,
-        ]);
-    }
-    private function createFile($request)
-    {
-        return TblFile::create([
-            'rapor_semester_9' => $request->hasFile('rapor_semester_9') ? $this->compressPDF($request->rapor_semester_9) : null,
-            'rapor_semester_10' => $request->hasFile('rapor_semester_10') ? $this->compressPDF($request->rapor_semester_10) : null,
-            'rapor_semester_11' => $request->hasFile('rapor_semester_11') ? $this->compressPDF($request->rapor_semester_11) : null,
-            'foto' => $request->hasFile('foto') ? $request->foto->store('uploads', 'public') : null,
-        ])->wasRecentlyCreated();
-    }
-    protected function compressPDF($file)
-    {
-        $originalPath = $file->getPathName();
-        $destinationPath = $file->getRealPath() . '_compressed.pdf'; // Modify this path as needed
-
-        $process = new Process([
-            'gs',
-            '-sDEVICE=pdfwrite',
-            '-dCompatibilityLevel=1.4',
-            '-dPDFSETTINGS=/screen',
-            '-dNOPAUSE',
-            '-dQUIET',
-            '-dBATCH',
-            '-sOutputFile=' . $destinationPath,
-            $originalPath,
-        ]);
-
-        $process->run();
-
-        if (!$process->isSuccessful()) {
-            throw new ProcessFailedException($process);
-        }
-
-        // After compression, save the file to your desired location
-        $finalPath = 'uploads/' . $file->hashName();
-        \Storage::disk('public')->put($finalPath, file_get_contents($destinationPath));
-
-        // Clean up temporary file
-        unlink($destinationPath);
-
-        return $finalPath;
-    }
-
-    private function createHasil($daftar)
-    {
-        return TblHasil::create([
-            'nis' => $daftar->id
-        ]);
-    }
-
-    public function hasil()
+                public function hasil()
     {
         $items = TblHasil::with(['tbl_peserta_ppdb'])->get();
         return view('home.hasil', compact('items'));
-
     }
 }
--- public/assets/js/daftar.js
+++ public/assets/js/daftar.js
@@ -1,4 +1,4 @@
-(function($) {
+(function ($) {
     var form = $("#signup-form");
 
     form.steps({
@@ -6,28 +6,28 @@
         bodyTag: "fieldset",
         transitionEffect: "fade",
         labels: {
-            previous: 'Previous',
-            next: 'Next',
-            finish: 'Submit',
-            current: ''
+            previous: "Sebelumnya",
+            next: "Selanjutnya",
+            finish: "Kirim",
+            current: "",
         },
         titleTemplate: '<h3 class="title">#title#</h3>',
         onStepChanging: function (event, currentIndex, newIndex) {
             return form.valid();
         },
-        onFinished: function(event, currentIndex) {
+        onFinished: function (event, currentIndex) {
             if (form.valid()) {
                 form.submit();
             } else {
                 form.find(":input.error:first").focus();
             }
-        }
+        },
     });
 
     form.validate({
         rules: {
-            nama_depan: 'required',
-            nama_belakang: 'required',
+            nama_depan: "required",
+            nama_belakang: "required",
             nisn: {
                 required: true,
                 digits: true,
@@ -40,55 +40,91 @@
                 required: true,
                 digits: true,
             },
-            jenis_kelamin: 'required',
-            agama: 'required',
-            tanggal_lahir: 'required',
-            tempat_lahir: 'required',
-            asal_sekolah: 'required',
-            nama_ayah: 'required',
-            id_pekerjaan_ayah: 'required',
+            jenis_kelamin: "required",
+            agama: "required",
+            tanggal_lahir: "required",
+            tempat_lahir: "required",
+            asal_sekolah: "required",
+            nama_ayah: "required",
+            id_pekerjaan_ayah: "required",
             no_telp_ayah: {
                 required: true,
                 digits: true,
             },
-            nama_ibu: 'required',
-            id_pekerjaan_ibu: 'required',
+            nama_ibu: "required",
+            id_pekerjaan_ibu: "required",
             no_telp_ibu: {
                 required: true,
                 digits: true,
             },
+            rapor_semester_9: {
+                required: true,
+            },
+            rapor_semester_10: {
+                required: true,
+            },
+            rapor_semester_11: {
+                required: true,
+            },
+            foto: {
+                required: true,
+            },
         },
         messages: {
-            ijasah: {
-                required: "Silakan unggah file ijasah Anda",
+            nama_depan: {
+                required: "Silakan masukkan nama depan Anda",
             },
-            kk: {
-                required: "Silakan unggah file Kartu Keluarga (KK) Anda",
+            nama_belakang: {
+                required: "Silakan masukkan nama belakang Anda",
             },
-            nama_ayah: {
-                required: "Silakan masukkan nama ayah Anda",
+            nisn: {
+                required: "Silakan masukkan NISN Anda",
+                digits: "Silakan masukkan hanya angka",
             },
-            id_pekerjaan_ayah: {
-                required: "Silakan pilih pekerjaan ayah Anda",
+            nik: {
+                required: "Silakan masukkan NIK Anda",
+                digits: "Silakan masukkan hanya angka",
             },
-            nama_ibu: {
-                required: "Silakan masukkan nama ibu Anda",
+            no_kk: {
+                required: "Silakan masukkan nomor KK Anda",
+                digits: "Silakan masukkan hanya angka",
             },
-            id_pekerjaan_ibu: {
-                required: "Silakan pilih pekerjaan ibu Anda",
+            jenis_kelamin: "Silakan pilih jenis kelamin Anda",
+            agama: "Silakan pilih agama Anda",
+            tanggal_lahir: "Silakan masukkan tanggal lahir Anda",
+            tempat_lahir: "Silakan masukkan tempat lahir Anda",
+            asal_sekolah: "Silakan masukkan nama sekolah asal Anda",
+            nama_ayah: "Silakan masukkan nama ayah Anda",
+            id_pekerjaan_ayah: "Silakan pilih pekerjaan ayah Anda",
+            no_telp_ayah: {
+                required: "Silakan masukkan nomor telepon ayah Anda",
+                digits: "Silakan masukkan nomor telepon yang valid",
             },
-            no_telp_ortu: {
-                required: "Silakan masukkan nomor telepon orang tua Anda",
-                number: "Silakan masukkan nomor telepon yang valid",
+            nama_ibu: "Silakan masukkan nama ibu Anda",
+            id_pekerjaan_ibu: "Silakan pilih pekerjaan ibu Anda",
+            no_telp_ibu: {
+                required: "Silakan masukkan nomor telepon ibu Anda",
+                digits: "Silakan masukkan nomor telepon yang valid",
+            },
+            rapor_semester_9: {
+                required: "Silakan unggah rapor Semester 9 Anda",
+            },
+            rapor_semester_10: {
+                required: "Silakan unggah rapor Semester 10 Anda",
+            },
+            rapor_semester_11: {
+                required: "Silakan unggah rapor Semester 11 Anda",
+            },
+            foto: {
+                required: "Silakan unggah foto Anda",
             },
         },
-        invalidHandler: function(event, validator) {
+        invalidHandler: function (event, validator) {
             var errors = validator.numberOfInvalids();
             if (errors) {
                 var firstInvalidElement = $(validator.errorList[0].element);
                 firstInvalidElement.focus();
             }
-        }
+        },
     });
-
 })(jQuery);
